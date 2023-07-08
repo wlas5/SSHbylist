4523e8f2fccfa7b9ca9413b498c5aeeaf1a9a147
wlas5 <87370007+wlas5@users.noreply.github.com>
2023-07-07 21:50:40 -0300

    Update join.sh

--- join.sh
+++ join.sh
@@ -2,11 +2,11 @@
 
 regex="\b[0-9]{4}\b" #expressão regular para verificar se ID passado
 		       # tá no formado correto
-if [[ $1 =~ $regex ]]
+if [[ $1 =~ $regex ]] #verifica se o parâmetro passado na execução do script tá no formato corrento (4 dígitos de 0 à 9)
 then
-	ip=$(cat ~/need/medidores.txt | grep $1 | awk '{ print $3 }')
-	user=$(cat ~/need/medidores.txt | grep $1 | awk '{ print $1 }')
-	if [[ $ip ]]
+	ip=$(cat ~/need/medidores.txt | grep $1 | awk '{ print $3 }') #captura o parâmetro da coluna 3 do arquivo medidores.txt (que corresponde ao ip)
+	user=$(cat ~/need/medidores.txt | grep $1 | awk '{ print $1 }') #captura o parâmetro da coluna 1 do arquivo medidores.txt (que corresponde ao usuário)
+	if [[ $ip ]] # se o comando cat devolver algo no ip, executa os comandos abaixo
 	then
         	echo "Medidor encontrado, acessando via ssh..."
         	sshpass -p "#senha_do_dispositivo" ssh $user@$ip
